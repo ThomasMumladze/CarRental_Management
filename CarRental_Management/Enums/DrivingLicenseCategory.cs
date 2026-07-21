65eabb8a6da1cf9e5bdc463dc1af4fdd1781c5b4
ThomasMumladze <98337658+ThomasMumladze@users.noreply.github.com>
2026-07-21 15:55:01 +0400

    add driving licese categories

---
+++ CarRental_Management/Enums/DrivingLicenseCategory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarRental_Management.Enums
+{
+    public enum DrivingLicenseCategory
+    {
+        None = 0,
+        A1   = 1 << 0,   // 1
+        A    = 1 << 1,   // 2
+        B1   = 1 << 2,   // 4
+        B    = 1 << 3,   // 8
+        BE   = 1 << 4,   // 16
+        C1   = 1 << 5,   // 32
+        C    = 1 << 6,   // 64
+        C1E  = 1 << 7,   // 128
+        CE   = 1 << 8,   // 256
+        D1   = 1 << 9,   // 512
+        D    = 1 << 10,  // 1024
+        D1E  = 1 << 11,  // 2048
+        DE   = 1 << 12,  // 4096
+        T    = 1 << 13
+    }
+}
