4cac3bb7766925764847ec266de83386fc75a44e
ThomasMumladze <98337658+ThomasMumladze@users.noreply.github.com>
2026-07-21 16:21:59 +0400

    add date conmtext

---
+++ CarRental_Management/Data/CarRentalDbContext.cs
@@ -0,0 +1,18 @@
+﻿using CarRental_Management.Entities;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace CarRental_Management.Data
+{
+    public class CarRentalDbContext : DbContext
+    {
+       public DbSet<Car> Cars { get; set; }
+       public DbSet<Customer> Customers { get; set; }
+       public DbSet<Rental> Rentals{ get; set; }
+
+        protected override void OnConfiguring (DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("");
+        }
+    }
+}
