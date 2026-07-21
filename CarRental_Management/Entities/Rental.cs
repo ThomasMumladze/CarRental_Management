a8a3e05d5ca23c0dd04174ef8464958b5e1027af
ThomasMumladze <98337658+ThomasMumladze@users.noreply.github.com>
2026-07-21 15:48:44 +0400

    updated rental entity

--- CarRental_Management/Entities/Rental.cs
+++ CarRental_Management/Entities/Rental.cs
@@ -1,10 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using CarRental_Management.Enums;
+using System.ComponentModel.DataAnnotations;
 
 namespace CarRental_Management.Entities
 {
     internal class Rental
     {
+        [Key]
+        public int Id { get; set; }
+        [Required, MaxLength(50)]
+        public string Brand { get; set; } = string.Empty;
+        [Required, MaxLength(50)]
+        public string Model { get; set; } = string.Empty;
+        [Required]
+        public int Year { get; set; }
+        [Required, MaxLength(50)]
+        public string LicensePlate { get; set; } = string.Empty;
+        [Required]
+        public decimal DailyPrice { get; set; } = decimal.MaxValue;
+        public CarCategory Category { get; set; }
+        public TransmissionType Transmission { get; set; }
+        public FuelType FuelType { get; set; }
+        public bool IsAvailable { get; set; }
+        public DateTime CreateAt { get; set; } = DateTime.Now;
+        public DateTime UpdatedAt { get; set; } = DateTime.Now;
+
     }
 }
