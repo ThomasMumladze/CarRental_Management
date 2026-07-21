f9eb8c30a0c3a93fd72abdfa7bd21b2954d4446a
ThomasMumladze <98337658+ThomasMumladze@users.noreply.github.com>
2026-07-21 17:04:10 +0400

    default logic of shiwng menu

--- CarRental_Management/Program.cs
+++ CarRental_Management/Program.cs
@@ -1,5 +1,4 @@
 ﻿using CarRental_Management.Enums;
-using System;
 using System.Text;
 
 namespace CarRental_Management
@@ -7,31 +6,99 @@ namespace CarRental_Management
     class Program
     {
         public static MenuOption _menuOptions;
+        public static MenuOption _menuOption;
         public static void Main(string[] args)
-        {   
+        {
             // Helps Read Georgian Alphabet
             Console.OutputEncoding = Encoding.UTF8;
             Console.InputEncoding = Encoding.UTF8;
 
-            Console.WriteLine("====================================\r\n       CAR RENTAL MANAGEMENT\r\n====================================\r\n");
-               
-            string[] MenuOptions = 
-                {
-                    "1. ავტომობილების მართვა",
-                    "2. მომხმარებლების მართვა",
-                    "3. ავტომობილის გაქირავება",
-                    "4. ავტომობილის დაბრუნება",
-                    "5. აქტიური გაქირავებების ნახვა",
-                    "6. გაქირავებების ისტორია",
-                    "7. ძებნა და ფილტრაცია",
-                    "8. სტატისტიკა",
-                    "0. პროგრამიდან გამოსვლა"
-                };
+            var app = new Program();
 
-                foreach (var item in MenuOptions)
-                {
-                    Console.WriteLine(item);
-                }           
+            app.Run();
         }
+
+        private void Run()
+        {
+            bool isRuning = true;
+
+            while (isRuning)
+            {
+                Console.Clear();
+                Console.WriteLine("====================================");
+                Console.WriteLine("       CAR RENTAL MANAGEMENT");
+                Console.WriteLine("====================================\n");
+
+                ShowMenu();
+
+                string input = Console.ReadLine();
+
+                if (!int.TryParse(input, out int choise) || !Enum.IsDefined(typeof(MenuOption), choise))
+                {
+                    Console.WriteLine("\nარასწორი მონაცემი! დააჭირეთ Enter-ს გასაგრძელებლად...");
+                    Console.ReadLine();
+                    continue;
+                }
+
+                _menuOption = (MenuOption)choise;
+                isRuning = HandleSelection();
+            }
+        }
+
+        //showing Menu Options
+        private void ShowMenu()
+        {
+            foreach (MenuOption menuOption in Enum.GetValues(typeof(MenuOption))) {
+                Console.WriteLine($"{(int)menuOption}. {menuOption.GetDescription()}");
+            }
+        }
+
+        private bool HandleSelection()
+        {
+            switch (_menuOption)
+            {
+                case MenuOption.ManageCars:
+                    Console.WriteLine("\n→ ავტომობილების მართვის მენიუ");
+                    break;
+
+                case MenuOption.ManageCustomers:
+                    Console.WriteLine("\n→ მომხმარებლების მართვის მენიუ");
+                    break;
+
+                case MenuOption.RentCar:
+                    Console.WriteLine("\n→ ავტომობილის გაქირავება");
+                    break;
+
+                case MenuOption.ReturnCar:
+                    Console.WriteLine("\n→ ავტომობილის დაბრუნება");
+                    break;
+
+                case MenuOption.ViewActiveRentals:
+                    Console.WriteLine("\n→ აქტიური გაქირავებები");
+                    break;
+
+                case MenuOption.RentalHistory:
+                    Console.WriteLine("\n→ გაქირავებების ისტორია");
+                    break;
+
+                case MenuOption.SearchAndFilter:
+                    Console.WriteLine("\n→ ძებნა და ფილტრაცია");
+                    break;
+
+                case MenuOption.Statistics:
+                    Console.WriteLine("\n→ სტატისტიკა");
+                    break;
+
+                case MenuOption.ExitProgram:
+                    Console.WriteLine("\nპროგრამა დასრულდა.");
+                    return false;
+            }
+
+            Console.WriteLine("\nდააჭირეთ Enter-ს გასაგრძელებლად...");
+            Console.ReadLine();
+            return true;
+        }
+
+
     }
 }
