1687d6245b15a102bd858fcacaa6d7a41c3136ea
ThomasMumladze <98337658+ThomasMumladze@users.noreply.github.com>
2026-07-23 21:14:12 +0400

    customer repositories

---
+++ CarRental_Management/Repositories/CustomerRepository.cs
@@ -0,0 +1,53 @@
+﻿using CarRental_Management.Data;
+using CarRental_Management.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarRental_Management.Repositories
+{
+    internal class CustomerRepository : ICustomerRepository
+    {
+        private readonly CarRentalDbContext _context;
+
+        public CustomerRepository(CarRentalDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<Customer> GetAll()
+        {
+            return _context.Customers.ToList();
+        }
+
+        public Customer GetById(int id)
+        {
+            return _context.Customers.FirstOrDefault(c => c.Id == id);
+        }
+
+        public void Add(Customer entity)
+        {
+            _context.Customers.Add(entity);
+        }
+
+        public void Update(Customer entity)
+        {
+            _context.Customers.Update(entity);
+        }
+
+        public void Delete(int id)
+        {
+            var customer = GetById(id);
+            if (customer != null)
+            {
+                _context.Customers.Remove(customer);
+            }
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+
+    }
+}
---
+++ CarRental_Management/Repositories/ICustomerRepository.cs
@@ -0,0 +1,17 @@
+﻿using CarRental_Management.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarRental_Management.Repositories
+{
+    public interface ICustomerRepository
+    {
+        List<Customer> GetAll();
+        Customer GetById(int id);
+        void Add(Customer entity);
+        void Update(Customer entity);
+        void Delete(int id);
+        void Save();
+    }
+}
