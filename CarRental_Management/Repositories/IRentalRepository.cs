802472816def3428ba21b2b3f521fc5f41cd6380
ThomasMumladze <98337658+ThomasMumladze@users.noreply.github.com>
2026-07-23 21:14:26 +0400

    rental repositories

---
+++ CarRental_Management/Repositories/IRentalRepository.cs
@@ -0,0 +1,17 @@
+﻿using CarRental_Management.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarRental_Management.Repositories
+{
+    internal interface IRentalRepository
+    {
+        List<Rental> GetAll();
+        Rental GetById(int id);
+        void Add(Rental entity);
+        void Update(Rental entity);
+        void Delete(int id);
+        void Save();
+    }
+}
---
+++ CarRental_Management/Repositories/RentalRepository.cs
@@ -0,0 +1,59 @@
+﻿using CarRental_Management.Data;
+using CarRental_Management.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarRental_Management.Repositories
+{
+    internal class RentalRepository : IRentalRepository
+    {
+        private readonly CarRentalDbContext _context;
+
+        public RentalRepository(CarRentalDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<Rental> GetAll()
+        {
+            return _context.Rentals
+                .Include(r => r.Car)
+                .Include(r => r.Customer)
+                .ToList();
+        }
+
+        public Rental GetById(int id)
+        {
+            return _context.Rentals
+                .Include(r => r.Car)
+                .Include(r => r.Customer)
+                .FirstOrDefault(r => r.Id == id);
+        }
+
+        public void Add(Rental entity)
+        {
+            _context.Rentals.Add(entity);
+        }
+
+        public void Update(Rental entity)
+        {
+            _context.Rentals.Update(entity);
+        }
+
+        public void Delete(int id)
+        {
+            var rental = _context.Rentals.FirstOrDefault(r => r.Id == id);
+            if (rental != null)
+            {
+                _context.Rentals.Remove(rental);
+            }
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+    }
+}
