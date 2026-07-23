273c0167588c20cfaf1ff9278d43d4121fa70c7a
ThomasMumladze <98337658+ThomasMumladze@users.noreply.github.com>
2026-07-23 21:14:44 +0400

    changed datacontext

--- CarRental_Management/Data/CarRentalDbContext.cs
+++ CarRental_Management/Data/CarRentalDbContext.cs
@@ -6,17 +6,52 @@ namespace CarRental_Management.Data
 {
     public class CarRentalDbContext : DbContext
     {
-       public DbSet<Car> Cars { get; set; }
-       public DbSet<Customer> Customers { get; set; }
-       public DbSet<Rental> Rentals{ get; set; }
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Rental> Rentals { get; set; }
 
-        protected override void OnConfiguring (DbContextOptionsBuilder optionsBuilder)
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Server=localhost;Database=CarRental_Management;Trusted_Connection=True;TrustServerCertificate=True;");
+            if (!optionsBuilder.IsConfigured)
+                optionsBuilder.UseSqlServer("Server=localhost;Database=CarRental_Management;Trusted_Connection=True;TrustServerCertificate=True;");
         }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.HasDefaultSchema("car_rental");
+
+            modelBuilder.Entity<Car>()
+            .HasIndex(c => c.LicensePlate)
+            .IsUnique();
+
+            modelBuilder.Entity<Customer>()
+                .HasIndex(c => c.PersonalNumber)
+                .IsUnique();
+
+            modelBuilder.Entity<Customer>()
+                .HasIndex(c => c.DrivingLicenseNumber)
+                .IsUnique();
+
+            // --- Decimal precision ---
+            modelBuilder.Entity<Car>()
+                .Property(c => c.DailyPrice)
+                .HasColumnType("decimal(18,2)");
+
+            modelBuilder.Entity<Rental>()
+                .Property(r => r.TotalPrice)
+                .HasColumnType("decimal(18,2)");
+
+            // --- One-to-Many  ---
+            modelBuilder.Entity<Rental>()
+                .HasOne(r => r.Car)
+                .WithMany(c => c.Rentals)
+                .HasForeignKey(r => r.CarId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Rental>()
+                .HasOne(r => r.Customer)
+                .WithMany(c => c.Rentals)
+                .HasForeignKey(r => r.CustomerId)
+                .OnDelete(DeleteBehavior.Restrict);
         }
     }
 }
