275c98200adc01b9adace393539ba68d7002df02
ThomasMumladze <98337658+ThomasMumladze@users.noreply.github.com>
2026-07-23 21:16:01 +0400

    customer services

---
+++ CarRental_Management/Services/CustomerService.cs
@@ -0,0 +1,30 @@
+﻿using CarRental_Management.Entities;
+using CarRental_Management.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarRental_Management.Services
+{
+    internal class CustomerService
+    {
+        private readonly ICustomerRepository _customerRepository;
+        private readonly IRentalRepository _rentalRepository;
+
+        public CustomerService(ICustomerRepository customerRepository, IRentalRepository rentalRepository)
+        {
+            _customerRepository = customerRepository;
+            _rentalRepository = rentalRepository;
+        }
+
+        public List<Customer> GetAllCustomers()
+        {
+            return _customerRepository.GetAll();
+        }
+
+        public Customer GetCustomerById(int id)
+        {
+            return _customerRepository.GetById(id);
+        }
+    }
+}
