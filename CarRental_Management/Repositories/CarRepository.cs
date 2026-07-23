6064294dd81431c5d6b5d2c6ba72637dc92f826a
ThomasMumladze <98337658+ThomasMumladze@users.noreply.github.com>
2026-07-23 21:13:59 +0400

    car repositories

---
+++ CarRental_Management/Repositories/CarRepository.cs
@@ -0,0 +1,52 @@
+﻿using CarRental_Management.Data;
+using CarRental_Management.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarRental_Management.Repositories
+{
+    internal class CarRepository : ICarRepository
+    {
+          private readonly CarRentalDbContext _context;
+
+        public CarRepository(CarRentalDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<Car> GetAll()
+        {
+            return _context.Cars.ToList();
+        }
+
+        public Car GetById(int id)
+        {
+            return _context.Cars.FirstOrDefault(c => c.Id == id);
+        }
+
+        public void Add(Car entity)
+        {
+            _context.Cars.Add(entity);
+        }
+
+        public void Update(Car entity)
+        {
+            _context.Cars.Update(entity);
+        }
+
+        public void Delete(int id)
+        {
+            var car = GetById(id);
+            if (car != null)
+            {
+                _context.Cars.Remove(car);
+            }
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+    }
+}
---
+++ CarRental_Management/Repositories/ICarRepository.cs
@@ -0,0 +1,17 @@
+﻿using CarRental_Management.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarRental_Management.Repositories
+{
+    public interface ICarRepository
+    {
+        List<Car> GetAll();
+        Car GetById(int id);
+        void Add(Car entity);
+        void Update(Car entity);
+        void Delete(int id);
+        void Save();
+    }
+}
