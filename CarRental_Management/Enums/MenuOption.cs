e8b125294bf2973bbf03a75db60d3d6370b893f0
ThomasMumladze <98337658+ThomasMumladze@users.noreply.github.com>
2026-07-24 12:47:11 +0400

    remove extension

--- CarRental_Management/Enums/MenuOption.cs
+++ CarRental_Management/Enums/MenuOption.cs
@@ -35,14 +35,4 @@ namespace CarRental_Management.Enums
         [Description("სტატისტიკა")]
         Statistics = 8
     }
-
-    public static class EnumExtensions
-    {
-        public static string GetDescription(this Enum value)
-        {
-            var field = value.GetType().GetField(value.ToString());
-            var attribute = field?.GetCustomAttribute<DescriptionAttribute>();
-            return attribute?.Description ?? value.ToString();
-        }
-    }
 }
