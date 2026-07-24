c7f6ec3b0b78cf451f8f292b16bf190503a01ba7
ThomasMumladze <98337658+ThomasMumladze@users.noreply.github.com>
2026-07-24 12:32:52 +0400

    add new enum

---
+++ CarRental_Management/Enums/CarMenuOption.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace CarRental_Management.Enums
+{
+    public enum CarMenuOption
+    {
+        [Description("მთავარ მენიუში დაბრუნება")]
+        Back = 0,
+
+        [Description("ავტომობილების სრული სია")]
+        ViewAll = 1,
+
+        [Description("ახალი ავტომობილის დამატება")]
+        Add = 2,
+
+        [Description("ავტომობილის დეტალების ნახვა")]
+        ViewDetails = 3,
+
+        [Description("ავტომობილის რედაქტირება")]
+        Edit = 4,
+
+        [Description("ავტომობილის წაშლა")]
+        Delete = 5,
+
+        [Description("ხელმისაწვდომი ავტომობილების ნახვა")]
+        ViewAvailable = 6
+    }
+}
