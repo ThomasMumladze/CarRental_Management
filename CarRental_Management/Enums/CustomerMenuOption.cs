b599e0ae1faaa02cc813e9c6a822843e5aafa107
ThomasMumladze <98337658+ThomasMumladze@users.noreply.github.com>
2026-07-24 12:48:28 +0400

    added new enum

---
+++ CarRental_Management/Enums/CustomerMenuOption.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+
+namespace CarRental_Management.Enums
+{
+    public enum CustomerMenuOption
+    {
+        [Description("მთავარ მენიუში დაბრუნება")]
+        Back = 0,
+
+        [Description("მომხმარებლების სრული სია")]
+        ViewAll = 1,
+
+        [Description("ახალი მომხმარებლის დამატება")]
+        Add = 2,
+
+        [Description("მომხმარებლის დეტალების ნახვა")]
+        ViewDetails = 3,
+
+        [Description("მომხმარებლის რედაქტირება")]
+        Edit = 4,
+
+        [Description("მომხმარებლის წაშლა")]
+        Delete = 5,
+
+        [Description("მომხმარებლის გაქირავებების ისტორია")]
+        RentalHistory = 6
+    }
+}
---
+++ CarRental_Management/Enums/SearchMenuOption.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel;
+
+
+namespace CarRental_Management.Enums
+{
+    public enum SearchMenuOption
+    {
+        [Description("მთავარ მენიუში დაბრუნება")]
+        Back = 0,
+
+        [Description("ავტომობილის ძებნა ბრენდის მიხედვით")]
+        SearchByBrand = 1,
+
+        [Description("ავტომობილის ძებნა მოდელის მიხედვით")]
+        SearchByModel = 2,
+
+        [Description("ავტომობილების ფილტრაცია კატეგორიის მიხედვით")]
+        FilterByCategory = 3,
+
+        [Description("ავტომობილების ფილტრაცია ფასის მიხედვით")]
+        FilterByPrice = 4,
+
+        [Description("ხელმისაწვდომი ავტომობილები")]
+        AvailableCars = 5,
+
+        [Description("მომხმარებლის ძებნა პირადი ნომრით")]
+        SearchCustomerByPersonalNumber = 6,
+
+        [Description("მომხმარებლის ძებნა ტელეფონის ნომრით")]
+        SearchCustomerByPhone = 7
+    }
+}
